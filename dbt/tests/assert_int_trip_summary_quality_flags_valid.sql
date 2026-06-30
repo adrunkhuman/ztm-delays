32327074784c9e832151343c641cf4a3a1522121
Łukasz <16039109+adrunkhuman@users.noreply.github.com>
2026-06-30 04:59:23 +0200

    Add completed trip fact (#47)

---
+++ dbt/tests/assert_int_trip_summary_quality_flags_valid.sql
@@ -0,0 +1,13 @@
+select quality_flag
+from {{ ref('int_trip_summary') }}, unnest(quality_flags) as quality_flag
+where gps_date = date('{{ var("processing_date", "1970-01-01") }}')
+  and quality_flag not in (
+    'missing_first_stop',
+    'missing_last_stop',
+    'low_stop_coverage',
+    'large_ping_gap',
+    'non_monotonic_stop_progression',
+    'impossible_speed_jump',
+    'extreme_delay',
+    'likely_wrong_trip_assignment'
+  )
