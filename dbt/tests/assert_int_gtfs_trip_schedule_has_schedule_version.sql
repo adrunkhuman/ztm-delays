ac274d6e1fb3f2f24cb92a145e44ec4380a81ea9
adrunkhuman <16039109+adrunkhuman@users.noreply.github.com>
2026-07-09 01:06:08 +0200

    Scope schedule-version test to processing date

--- dbt/tests/assert_int_gtfs_trip_schedule_has_schedule_version.sql
+++ dbt/tests/assert_int_gtfs_trip_schedule_has_schedule_version.sql
@@ -13,6 +13,7 @@ left join {{ ref('dim_schedule_version') }} as dim_schedule_version
     and trip_schedule.schedule_day_type = dim_schedule_version.schedule_day_type
     and trip_schedule.processing_date between dim_schedule_version.valid_from_date
     and coalesce(dim_schedule_version.valid_to_date, date '9999-12-31')
+where trip_schedule.processing_date = date('{{ var("processing_date") }}')
 group by
     trip_schedule.gtfs_snapshot_id,
     trip_schedule.processing_date,
